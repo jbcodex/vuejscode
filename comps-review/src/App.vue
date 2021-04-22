464ca4950cab88559632b528c0f885249700db84
Jarbas Bento <jbdev@tutanota.com>
2021-04-22 15:45:03 -0300

    Review de componentes

---
+++ comps-review/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <p>Comp PAI</p>
+    <p>{{nome}}</p>
+    <button @click="setName">Set name</button>
+    <User 
+    :user = "nome" 
+    @userChange = "nome = $event"
+    :reiniciarFn="reiniciarNome"/>
+  </div>
+</template>
+
+<script>
+import User from "@/components/User.vue";
+export default {
+  components: {
+    User,
+  },
+  data(){
+    return{
+      nome:'Jarbolha'
+    }
+  },
+  methods:{
+    setName(){
+      this.nome = 'Maria'
+    },
+     reiniciarNome(){
+      this.nome = "Renata"
+    }
+  }
+};
+</script>
+
+<style>
+.container {
+  width: 150px;
+  text-align: center;
+  background-color: rgb(30, 189, 210);
+  padding: 10px;
+  border-radius: 5px;
+   font-family: Arial, Helvetica, sans-serif;
+}
+</style>>
+   
---
+++ comps-review/src/components/User.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <p>Usuário</p>
+    <p>{{ user }}</p>
+    <button @click="change">:::Restart:::</button>
+    <button @click="reiniciarFn()">Restart Callback</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: String,
+      //required:true,
+      default: "Anônimo",
+    },
+
+    reiniciarFn: {
+      type: Function,
+    },
+  },
+  methods: {
+    change() {
+      this.user = "Marcinho";
+      this.$emit("userChange", this.user);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  background-color: rgb(149, 238, 127);
+  padding: 10px;
+  border-radius: 5px;
+  font-family: Arial, Helvetica, sans-serif;
+  margin-top: 20px;
+}
+</style>
